e7224fc445dd1f16b89c5b67347467b60242630a
Andrew Nguyen <andrewnguyen17678@gmail.com>
2022-08-19 00:48:26 -0700

    added angular components

--- post-it/src/app/app.module.ts
+++ post-it/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoteComponent } from './note/note.component';
+import { NotesComponent } from './notes/notes.component';
+import { ModalComponent } from './modal/modal.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CreateNoteComponent } from './create-note/create-note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
+    NotesComponent,
+    ModalComponent,
+    NavbarComponent,
+    CreateNoteComponent
   ],
   imports: [
     BrowserModule
