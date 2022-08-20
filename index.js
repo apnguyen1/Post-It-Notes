18deab63418a14d913cea7f9ad0527783a408a7f
Andrew Nguyen <andrewnguyen17678@gmail.com>
2022-08-19 20:52:11 -0700

    fixed deployment 1

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 import express from 'express';
 import cors from 'cors';
 import {router} from "./routes/updateNotes.js";
+import * as path from "path";
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
 
 const port = process.env.PORT || 5000;
 const DB = "mongodb+srv://andrew1767:Andrew1767@notes.o0una4b.mongodb.net/?retryWrites=true&w=majority";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express()
 .use(cors())
@@ -12,7 +17,11 @@ const app = express()
 .use(express.urlencoded({extended: true}))
 .use("/api", router)
 
-app.use("/", express.static('post-it/dist/post-it'));
+app.use(express.static('post-it/dist/post-it'));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'post-it/dist/post-it/index.html'));
+});
 
 
 
